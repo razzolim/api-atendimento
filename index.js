359bde39859f4179662573517ac6564639ef2caf
Renan Azzolim <azzolimrenan2@gmail.com>
2020-04-26 21:47:53 -0300

    post atendimento + config db

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = express();
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
-
-app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'));
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log('conectado com sucesso');
+        Tabelas.init(conexao);
+        
+        const app = customExpress();
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+});
